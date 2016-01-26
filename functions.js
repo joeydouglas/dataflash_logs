5830c9fe8498fccf2531cc80be12ee847496a72e
root <joey.douglas@gmail.com>
2016-01-26 15:47:36 -0600

    initial commit

---
+++ functions.js
@@ -0,0 +1,11 @@
+var getProps = function (obj) {
+    for(var i = 0, l = dataLength, i < l, i++) {
+                console.log(property + "=" + obj.data[property]);
+
+                document.getElementById("demo").innerHTML =
+                property +  "=" + obj.data[property];
+
+
+
+    }
+}
---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+
+
+
+function parseUpload(text) {
+
+// static data. eventually this will be fed from the JSON log file
+//var text = '{"meta": {"timestamp": 1413680662.120, "type": "IMU"}, "data": {"TimeMS": 313230, "AccY": 0.013031154870986938, "AccX": -0.15820294618606567, "AccZ": -9.863511085510254, "GyrZ": 0.01947564072906971, "GyrX": 0.0015512341633439064, "GyrY": 0.0040904320776462555}}';
+
+// create object from a line in the log file
+var obj = JSON.parse(text);
+
+//create array and fill it w/ tags from the object so key names can be referenced in the output to Influxdb.
+var arr = []
+
+for (var property in obj.data) {
+    //console.log(property + "=" + obj.data[property]);
+    arr.push(property + "=" + obj.data[property]);
+}
+
+arr.toString();
+
+var dataString = arr.toString();
+
+
+//remove decimal from Linux Epoch time. Time precision in MS will be specified for Influxdb.
+var newTimestamp = (obj.meta.timestamp + '').replace('.', '');
+
+
+//add additional zero to timestamp if it's not 13 digits long
+if (newTimestamp.length < 13) {
+    newTimestamp = newTimestamp + "0";
+}
+
+
+var dbString1 = "http://" + server + ":" + port + "/write?db=" + database + "&precision=ms";
+var dbString2 = obj.meta.type + "," + logInfoArr[0] + "," + logInfoArr[1] + " " + dataString + " " + newTimestamp;
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", dbString1, true);
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+
+xhr.send(dbString2);
+//console.log("dbString2= " + dbString2);
+
+//console.log("'http://" + server + ":" + port + "/write?db=" + database + "' --data-binary " + obj.meta.type + "," + logInfoArr[0] + "," + logInfoArr[1] + " " + dataString + " " + obj.meta.timestamp);
+//console.dir(obj)
+}
